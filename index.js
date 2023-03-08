4c4c8a621688b094c3e8e26812f3510e0f5c60a4
Aadil Ahmed <aadilahmed0@gmail.com>
2023-03-08 00:25:31 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const newArray = [];
+    for(let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return newArray;
+}
+
+function countDown(number) {
+    let counter = number;
+    while(counter >= 0) {
+        console.log(counter--);
+    }
+}
